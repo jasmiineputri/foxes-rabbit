1112d2070b0e358bb24f10ff17444e695ba7c88d
jasmiineputri <162336027+jasmiineputri@users.noreply.github.com>
2024-12-05 14:46:38 +0700

    Create Rabbit.java

---
+++ Rabbit.java
@@ -0,0 +1,11 @@
+public class Rabbit extends Animal {
+    @Override
+    public String move() {
+        return "Rabbit hops around.";
+    }
+
+    @Override
+    public String eat() {
+        return "Rabbit eats grass.";
+    }
+}
