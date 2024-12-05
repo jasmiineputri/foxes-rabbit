10ba2d6dbdcf9eb58d9e7f14e4eb21bee855a196
jasmiineputri <162336027+jasmiineputri@users.noreply.github.com>
2024-12-05 14:47:03 +0700

    Create Fox.java

---
+++ Fox.java
@@ -0,0 +1,11 @@
+public class Fox extends Animal {
+    @Override
+    public String move() {
+        return "Fox runs swiftly.";
+    }
+
+    @Override
+    public String eat() {
+        return "Fox eats rabbits.";
+    }
+}
